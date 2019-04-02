12bb372011c36fded32c72534e14a2dd6aaccaed
Shraddha Thakkar <st746@njit.edu>
2019-04-01 23:55:56 -0400

    test data has been created

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+        return view('home');
+    }
+    public function profile()
+
+    {
+
+
+
+       // $users = User::all();
+        $users = User::where('id',1)
+            ->orderBy('name','desc')
+            ->take(10)
+            ->get();
+        dd($users->count());
+        return view('profile',['users' => $users]);
+    }
+
+
+
+}
